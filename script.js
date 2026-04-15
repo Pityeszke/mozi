e8c0ac411128d05639c090d12f9d312d9f22c7f8
Pityeszke <fazekaspisti7@gmail.com>
2026-04-15 14:18:23 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function betoltes(){
+    let div = document.getElementById("tartalom");
+    for (let i = 0; i < filmek.length; i++) {
+        let film = filmek[i];
+        let p = document.createElement("p");
+        p.textContent = film.cim;
+        div.appendChild(p);
+    }
+}
+addEventListener("DOMContentLoaded", betoltes);
